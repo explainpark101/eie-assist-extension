a610d41e8d471da94468817b4b74d62b84c82d03
Park JaeHyung <explainpark101@gmail.com>
2022-10-17 15:39:08 +0900

    반 수정 기능 개선

--- classregister/classregister.js
+++ classregister/classregister.js
@@ -1,16 +1,31 @@
 let studentsUrl = "https://eie.co.kr/eielms/pages/modal/academy/class.student.select2.php?page=";
 let 원본URL = "https://eie.co.kr/eielms/pages/modal/academy/class.student.select2.php?page=12&method=&search_where=&search_keyword=";
 
+function removeCharactersFromArray(target,removeList){
+
+    for(remove of removeList){
+        target = target.replace(remove,"");
+    }
+    return target;
+}
+
 function 학생목록불러오기(){
     let iframe = document.querySelector("div.modal iframe");
     iframe.addEventListener("load",()=>{
         let document = iframe.contentWindow.document;
-        let last_item = document.body.querySelector("#table2_last");
+        let last_item = document.querySelector("a.pg_end");
         var urlPoint = last_item.href.indexOf('page=');
-        let last_page_num = last_item.href.slice(urlPoint+5,-1).replace("&method=r&search_where=&search_keyword","");
+        let last_page_num = removeCharactersFromArray(last_item.href.slice(urlPoint+5,-1),[
+            "&method=r","&search_where=","&search_keyword=","&search_keyword","&method=u"
+        ]);
+        console.log(last_page_num); 
+
+        last_page_num = new Number(last_page_num);
         let currentTable = document.querySelector(".c-table tbody");
+        let ajaxArray = new Array();
         for(let page_num = 2; page_num <= last_page_num; page_num++){
-            $.ajax({
+            console.log(`${page_num} 페이지 학생 로딩중!`)
+            let ajaxObj = $.ajax({
                 url: studentsUrl+page_num,
                 contenttype: "html",
                 method:"GET",
@@ -25,6 +40,7 @@ function 학생목록불러오기(){
                     // console.log("완료!");
                 }
             })
+            ajaxArray.push(ajaxObj);
         }
         // 검색기능 구현하기
         let searchbox = document.querySelector("input[name=search_keyword]");
@@ -34,9 +50,12 @@ function 학생목록불러오기(){
         searchbox.addEventListener("keyup",()=>{
             iframeSearchbox(searchbox, iframe);
         });
-        document.querySelector(".table_paginate").parentElement.parentElement.remove();
-        document.querySelector(".c-table").insertAdjacentHTML("afterend", `<button type="button" id="select_ok" class="btn btn-sm btn-primary">학생 등록</button>`);
 
+        $.when(ajaxArray).then(()=>{
+            document.querySelector(".table_paginate").parentElement.parentElement.remove();
+            document.querySelector(".c-table").insertAdjacentHTML("afterend", `<button type="button" id="select_ok" class="btn btn-sm btn-primary">학생 등록</button>`);    
+        })
+        
 
     })
 }
@@ -55,6 +74,45 @@ function iframeSearchbox(searchbox, iframe){
     })
 }
 
+function addDeleteToStudent(){
+    let alltimesBox = document.querySelectorAll("span.q-updown");
+    alltimesBox.forEach(box => {
+        box.style.display = "";
+    })
+}
+
+const appendDeletebtnTeacher = ()=>{
+    document.querySelector(`button[data-modal-title="강사 선택"]`).insertAdjacentHTML("beforebegin",`
+    <button type="button" id="teacher_add_del" class="btn btn-eie-assist" style="display: inline-block;">강사 삭제 추가</button>
+    `)
+    document.querySelector("button#teacher_add_del").addEventListener("click", (event)=>{
+        addDeleteToTeacher();
+    })
+}
+const appendDeletebtnStudent = ()=>{
+    document.querySelector(`button[data-modal-title="학생 선택"]`).insertAdjacentHTML("beforebegin",`
+    <button type="button" id="student_add_del" class="btn btn-eie-assist" style="display: inline-block;">학생 삭제 추가</button>
+    `)
+    document.querySelector("button#student_add_del").addEventListener("click", (event)=>{
+        addDeleteToStudent();
+    })
+}
+
+const deleteteacherWithButton = (delbtn)=>{
+    let teacherForm = document.querySelector("div.teacher_input");
+    let input = teacherForm.querySelector(`input[id=${delbtn.parentElement.dataset.id}]`);
+    input.remove();
+}
+function addDeleteToTeacher(){
+    let allTeacherRows = document.querySelectorAll("ul#teacher_list > li");
+    allTeacherRows.forEach(row => {
+        if(row.querySelector("span.q-updown") == undefined)
+            row.insertAdjacentHTML("beforeend", `<span class="q-updown"><i class="ti-close s-del"></i></span>`);
+        let delbtn = row.querySelector("i");
+        delbtn.addEventListener("click", (event)=>{event.target});
+    })
+}
+
 /*
 모달창에 대해서 함수를 구현해야함. 다음 질의를 참고해야함.
 */
@@ -62,6 +120,13 @@ function iframeSearchbox(searchbox, iframe){
 
 document.querySelector("button[data-modal-title='학생 선택']").addEventListener("click", ()=>{
     학생목록불러오기();
+})
+
+window.addEventListener("load", ()=>{
+    addDeleteToStudent();
+    addDeleteToTeacher();
+    appendDeletebtnTeacher();
+    appendDeletebtnStudent();
 })
 
 document.querySelector(".modal-view").addEventListener("loaded.bs.modal",function(e){
